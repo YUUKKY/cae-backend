dc2d22d55f263e26b4e69eae3983d0ca6389b718
YUUKKY <43734044+YUUKKY@users.noreply.github.com>
2025-01-06 19:28:57 +0800

    init config-server

--- src/main/java/com/huawei/cae/Application.java
+++ src/main/java/com/huawei/cae/Application.java
@@ -3,9 +3,11 @@ package com.huawei.cae;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @MapperScan(basePackages = "com.huawei.cae.mapper")
 @SpringBootApplication
+@EnableConfigServer
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
